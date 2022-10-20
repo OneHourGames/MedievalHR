ca68a8d4d094f08566a3337be85ca89b6151e9e9
Zachary Foutz <zach@boomlearning.com>
2022-10-20 15:52:21 -0700

    * made every scenario have the ability to "complete" using an html inject into the srcdoc

--- local.js
+++ local.js
@@ -3,6 +3,9 @@ const scenarioFrame = document.querySelector('#scenarioFrame');
 const defaultScenarioSender = "no-reply@resolutions.rescorp";
 const missingScenarioUrl = "./scenarios/blankScenario.html";
 
+// rplc8 list of shownScenarios into #inbox
+const inbox = sleepless.rplc8("#r8_scenario");
+
 /**
  * @typedef {Object} Scenario
  * @property {string} name - The name of the scenario
@@ -11,7 +14,7 @@ const missingScenarioUrl = "./scenarios/blankScenario.html";
  * @property {string} sender - The sender of the scenario (defaults to defaultScenarioSender)
  * @property {boolean} shown - Whether the scenario should be shown in the inbox (defaults to false)
  * @property {boolean} completed - Whether the scenario has been completed (defaults to false)
- * @property {boolean} completed - Whether the scenario has been completed (defaults to false)
+ * @property {boolean} read - Whether the scenario has been clicked on (defaults to false)
  */
 
 /**
@@ -24,19 +27,40 @@ const scenarios = [
         url: './scenarios/intro/index.html',
         sender: defaultScenarioSender,
         shown: true,
-        completed: true
+        completed: false,
+        read: false,
     },
     {
         name: 'lunoids',
         description: 'Moronic creatures that produce excellent milk need your help!',
         url: './scenarios/lunoids/index.html',
         sender: defaultScenarioSender,
-        shown: true,
-        completed: false
+        shown: false,
+        completed: false,
+        read: false,
     },
 ];
 
-let currentScenario = null;
+const getGameSave = function()
+{
+    const gameSave = localStorage.getItem('gameSave');
+    if (gameSave && gameSave !== 'undefined')
+    {
+        return JSON.parse(gameSave);
+    }
+    
+    saveGame(scenarios);
+    return JSON.parse(localStorage.getItem('gameSave'));
+}
+
+const saveGame = function(game)
+{
+    if(!game)
+    {
+        return false;
+    }
+    localStorage.setItem('gameSave', JSON.stringify(game));
+}
 
 /**
  * @summary Loads the scenario with the given name
@@ -44,30 +68,45 @@ let currentScenario = null;
  */
 const loadScenario = function(url)
 {
-    const nextScenario = scenarios.find(scenario => scenario.url === url);
+    const nextScenario = getGameSave().find(scenario => scenario.url === url);
+    // console.log('loading next scenario', nextScenario);
     if (nextScenario)
     {
-        currentScenario = nextScenario;
         fetch(nextScenario.url)
         .then(response => response.text())
         .then(html =>
         {
             // console.log('loaded scenario', nextScenario.name, html);
+            const scenarioInjectionHtml = 
+            `
+                const currentScenario = ${JSON.stringify(nextScenario)};
+                const completeCurrentScenario = function()
+                {
+                    window.parent.postMessage({
+                        action: "completeScenario",
+                        scenario: ${JSON.stringify(nextScenario)},
+                    }, "*");
+                }
+            `;
+            
+            html = html.replace(/__inject__/g, scenarioInjectionHtml);
+            // console.log(html);
             scenarioFrame.srcdoc = html;
+            // scenarioFrame.contentWindow.postMessage({action: "setCurrentScenario", currentScenario: nextScenario}, '*');
+            
+            const gameSave = getGameSave();
         });
+
+        updateInbox();
     }
 }
 
-
-document.addEventListener('DOMContentLoaded', () => {
-    loadScenario(missingScenarioUrl);
+const updateInbox = function()
+{
+    const gameSave = getGameSave();
+    const shownScenarios = gameSave.filter(scenario => scenario.shown).reverse();
     
-    // get a list of scenarios that are shown
-    const shownScenarios = scenarios.filter(scenario => scenario.shown).reverse();
-
-    // rplc8 list of shownScenarios into #inbox
-    const inbox = sleepless.rplc8("#r8_scenario");
-
+    // console.log('shownScenarios', shownScenarios);
     inbox.update(shownScenarios, function(element, data, index)
     {
         // add completed class if scenario is completed
@@ -75,15 +114,86 @@ document.addEventListener('DOMContentLoaded', () => {
         {
             element.classList.add('completed');
         }
-        
+
+        // add isRead class if scenario is read
+        if (data.read)
+        {
+            element.classList.add('isRead');
+        }
+
         // add event listener to each scenario in the inbox to load the scenario when clicked
         element.addEventListener('click', () =>
         {
-            sleepless.QS('#inbox .scenario').forEach(scenario => scenario.classList.remove('active'));
-            // console.log(element, data);
-            loadScenario(data?.url || missingScenarioUrl);
+            // console.log(`clicked on scenario ${data.name}`);
+            
+            data.read = true;
+            element.classList.add('isRead');
             element.classList.add('active');
+
+            saveGame(gameSave);
+            loadScenario(data?.url || missingScenarioUrl);
         });
     });
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+    // get a list of scenarios that are shown
+    const gameSave = getGameSave();
+    const shownScenarios = gameSave.filter(scenario => scenario.shown);
+    
+    // get the next uncompleted scenario that is also shown
+    
+    updateInbox();
 });
 
+
+/**
+ * @summary Marks the current scenario as completed, then loads the next scenario that is shown and not completed yet
+ */
+const loadNextScenario = function(currentScenario)
+{
+    const gameSave = getGameSave();
+    const currentScenarioFromGameSave = gameSave.find(scenario => scenario.url === currentScenario.url);
+    
+    // console.log(currentScenarioFromGameSave);
+    
+    if(currentScenario && currentScenarioFromGameSave)
+    {
+        currentScenarioFromGameSave.completed = true;
+        saveGame(gameSave);
+    }
+
+    const nextScenario = gameSave.find(scenario => scenario.shown === false && scenario.completed === false );
+    if(nextScenario)
+    {
+        nextScenario.shown = true;
+    }
+
+    saveGame(gameSave);
+    updateInbox();
+    loadScenario(currentScenario?.url);
+}
+
+
+window.addEventListener('message', (message) => {
+    
+    const action = message.data?.action || null;
+    const scenario = message.data?.scenario || null;
+    
+    if (action)
+    {
+        if(!scenario)
+        {
+            console.error('No scenario specified');
+            return false;
+        }
+
+        // console.log(action, message);
+        if (action === 'completeScenario')
+        {
+            loadNextScenario(scenario);
+        }
+    }
+});
